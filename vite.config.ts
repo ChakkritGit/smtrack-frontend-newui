971eb960cfd810fc06d50ec26d6ab379b80b13cb
ChakkritGit <nongtonnee@gmail.com>
2025-04-11 10:13:36 +0700

    Comment out rollupOptions and manualChunks logic in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -17,57 +17,57 @@ export default defineConfig({
     commonjsOptions: {
       transformMixedEsModules: true
     },
-    rollupOptions: {
-      output: {
-        manualChunks (id) {
-          const normalizedId = id.split(path.sep).join('/')
+    // rollupOptions: {
+    //   output: {
+    //     manualChunks (id) {
+    //       const normalizedId = id.split(path.sep).join('/')
 
-          // if (normalizedId.includes('node_modules')) {
-          //   return 'vendor'
-          // }
+    //       if (normalizedId.includes('node_modules')) {
+    //         return 'vendor'
+    //       }
 
-          if (normalizedId.includes('/src/components/')) {
-            const parts = normalizedId.split('/src/components/')[1].split('/')
-            const folder = parts[0]
-            return `components/${folder}`
-          }
+    //       if (normalizedId.includes('/src/components/')) {
+    //         const parts = normalizedId.split('/src/components/')[1].split('/')
+    //         const folder = parts[0]
+    //         return `components/${folder}`
+    //       }
 
-          if (normalizedId.includes('/src/pages/')) {
-            const parts = normalizedId.split('/src/pages/')[1].split('/')
-            const folder = parts[0]
-            return `pages/${folder}`
-          }
+    //       if (normalizedId.includes('/src/pages/')) {
+    //         const parts = normalizedId.split('/src/pages/')[1].split('/')
+    //         const folder = parts[0]
+    //         return `pages/${folder}`
+    //       }
 
-          if (normalizedId.includes('/src/constants/')) {
-            const parts = normalizedId.split('/src/constants/')[1].split('/')
-            const folder = parts[0]
-            return `constants/${folder}`
-          }
+    //       if (normalizedId.includes('/src/constants/')) {
+    //         const parts = normalizedId.split('/src/constants/')[1].split('/')
+    //         const folder = parts[0]
+    //         return `constants/${folder}`
+    //       }
 
-          if (normalizedId.includes('/src/middleware/')) {
-            const parts = normalizedId.split('/src/middleware/')[1].split('/')
-            const folder = parts[0]
-            return `middleware/${folder}`
-          }
+    //       if (normalizedId.includes('/src/middleware/')) {
+    //         const parts = normalizedId.split('/src/middleware/')[1].split('/')
+    //         const folder = parts[0]
+    //         return `middleware/${folder}`
+    //       }
 
-          if (normalizedId.includes('/src/routes/')) {
-            const parts = normalizedId.split('/src/routes/')[1].split('/')
-            const folder = parts[0]
-            return `routes/${folder}`
-          }
-        },
-        assetFileNames: assetInfo => {
-          const name = assetInfo.name ?? 'asset'
-          if (name.endsWith('.css')) {
-            if (name.includes('component-')) return 'component/[name][extname]'
-            if (name.includes('page-')) return 'page/[name][extname]'
-            return 'style/[name][extname]'
-          }
+    //       if (normalizedId.includes('/src/routes/')) {
+    //         const parts = normalizedId.split('/src/routes/')[1].split('/')
+    //         const folder = parts[0]
+    //         return `routes/${folder}`
+    //       }
+    //     },
+    //     assetFileNames: assetInfo => {
+    //       const name = assetInfo.name ?? 'asset'
+    //       if (name.endsWith('.css')) {
+    //         if (name.includes('component-')) return 'component/[name][extname]'
+    //         if (name.includes('page-')) return 'page/[name][extname]'
+    //         return 'style/[name][extname]'
+    //       }
 
-          return 'assets/[name][extname]'
-        }
-      }
-    }
+    //       return 'assets/[name][extname]'
+    //     }
+    //   }
+    // }
   },
   server: {
     port: 12345,
