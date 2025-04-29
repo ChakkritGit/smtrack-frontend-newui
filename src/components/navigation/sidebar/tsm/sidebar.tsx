3a8b4c828ebc6a71aec6b1f30906c3a29b9928e4
ChakkritGit <nongtonnee@gmail.com>
2025-04-29 10:46:20 +0700

    Update image tag to 2.0.0b17 and adjust resource limits in deployment configuration

--- src/components/navigation/sidebar/tsm/sidebar.tsx
+++ src/components/navigation/sidebar/tsm/sidebar.tsx
@@ -263,7 +263,7 @@ const Sidebar = () => {
                 isExpand ? 'text-center' : 'text-right'
               } hover:underline cursor-pointer`}
             >
-              Version 2.0.0b16f
+              Version 2.0.0b17
             </Link>
           </div>
         </div>
