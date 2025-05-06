ede66e51498ab682760ef6302b6c5f27ee138996
ChakkritGit <nongtonnee@gmail.com>
2025-05-06 10:09:30 +0700

    Update image tag to 2.0.0b17b in workflows and sidebar components

--- src/components/navigation/sidebar/tsm/sidebar.tsx
+++ src/components/navigation/sidebar/tsm/sidebar.tsx
@@ -263,7 +263,7 @@ const Sidebar = () => {
                 isExpand ? 'text-center' : 'text-right'
               } hover:underline cursor-pointer`}
             >
-              Version 2.0.0b17a
+              Version 2.0.0b17b
             </Link>
           </div>
         </div>
