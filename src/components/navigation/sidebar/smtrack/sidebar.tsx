8cebcdb35a58db7a938c663134f2043413156d3e
ChakkritGit <nongtonnee@gmail.com>
2025-05-16 09:58:19 +0700

    Refactor code structure for improved readability and maintainability

--- src/components/navigation/sidebar/smtrack/sidebar.tsx
+++ src/components/navigation/sidebar/smtrack/sidebar.tsx
@@ -339,7 +339,7 @@ const Sidebar = () => {
                 isExpand ? 'text-center' : 'text-right'
               } hover:underline cursor-pointer`}
             >
-              Version 2.0.0b20d
+              Version 2.0.0b20e
             </Link>
           </div>
         </div>
