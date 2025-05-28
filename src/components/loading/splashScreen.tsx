8b682734879bb24118b4823812d53c2a59dea14c
ChakkritGit <nongtonnee@gmail.com>
2025-05-28 12:42:34 +0700

    fix: update version tag to 2.0.0b25a in sidebar component; add theme mode to SplashScreen

--- src/components/loading/splashScreen.tsx
+++ src/components/loading/splashScreen.tsx
@@ -1,8 +1,16 @@
+import { RootState } from '../../redux/reducers/rootReducer'
+import { useSelector } from 'react-redux'
+
 const SplashScreen = () => {
+  const { themeMode } = useSelector((state: RootState) => state.utils)
+
   return (
-    <div className='w-full h-screen flex flex-col gap-3 items-center justify-center'>
-      <span className="text-4xl md:text-5xl font-medium">SMTrack+</span>
-      <span className="font-medium">Temperature Tracking</span>
+    <div
+      className='w-full h-screen flex flex-col gap-3 items-center justify-center'
+      data-theme={themeMode}
+    >
+      <span className='text-4xl md:text-5xl font-medium'>SMTrack+</span>
+      <span className='font-medium'>Temperature Tracking</span>
     </div>
   )
 }
