9e6afc2c315ccc818a80008d5a58fdf3cb1f7de7
ChakkritGit <nongtonnee@gmail.com>
2025-05-28 12:55:32 +0700

    fix: update version tag to 2.0.0b25b in sidebar components; add policies page to robots.txt and sitemap

--- src/components/navigation/sidebar/tsm/sidebar.tsx
+++ src/components/navigation/sidebar/tsm/sidebar.tsx
@@ -276,7 +276,7 @@ const Sidebar = () => {
                 isExpand ? 'text-center' : 'text-right'
               } hover:underline cursor-pointer`}
             >
-              Version 2.0.0b25a
+              Version 2.0.0b25b
             </Link>
           </div>
         </div>
