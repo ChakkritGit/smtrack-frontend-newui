8a17d5bf9de5b7065acc6de826c6198eb23a1adf
ChakkritGit <nongtonnee@gmail.com>
2025-05-28 13:03:41 +0700

    fix: update version tag to 2.0.0b25c in sidebar components; enhance theme handling in SplashScreen

--- src/components/loading/splashScreen.tsx
+++ src/components/loading/splashScreen.tsx
@@ -1,8 +1,44 @@
+import { useEffect } from 'react'
 import { RootState } from '../../redux/reducers/rootReducer'
 import { useSelector } from 'react-redux'
+import { getOKLCHColor } from '../../constants/utils/color'
 
 const SplashScreen = () => {
   const { themeMode } = useSelector((state: RootState) => state.utils)
+  const system = window.matchMedia('(prefers-color-scheme: dark)').matches
+    ? 'dark'
+    : 'light'
+
+  useEffect(() => {
+    const htmlElement = document.documentElement
+    htmlElement.setAttribute('data-theme', themeMode)
+    const themeColorMetaTag = document.querySelector('meta[name="theme-color"]')
+    const currentColor = getOKLCHColor(themeMode, system)
+
+    if (themeColorMetaTag) {
+      themeColorMetaTag.setAttribute('content', currentColor)
+    } else {
+      const newMetaTag = document.createElement('meta')
+      newMetaTag.setAttribute('name', 'theme-color')
+      newMetaTag.setAttribute('content', currentColor)
+      document.head.appendChild(newMetaTag)
+    }
+
+    const statusBarMetaTag = document.querySelector(
+      'meta[name="apple-mobile-web-app-status-bar-style"]'
+    )
+    if (statusBarMetaTag) {
+      statusBarMetaTag.setAttribute('content', currentColor)
+    } else {
+      const newStatusBarMetaTag = document.createElement('meta')
+      newStatusBarMetaTag.setAttribute(
+        'name',
+        'apple-mobile-web-app-status-bar-style'
+      )
+      newStatusBarMetaTag.setAttribute('content', currentColor)
+      document.head.appendChild(newStatusBarMetaTag)
+    }
+  }, [themeMode, system])
 
   return (
     <div
@@ -10,7 +46,7 @@ const SplashScreen = () => {
       data-theme={themeMode}
     >
       <span className='text-4xl md:text-5xl font-medium'>SMTrack+</span>
-      <span className='font-medium'>Temperature Tracking</span>
+      <span className='font-medium'>Siamatic Co. Ltd</span>
     </div>
   )
 }
