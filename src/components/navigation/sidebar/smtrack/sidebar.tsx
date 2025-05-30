31830e435d86c87c957cafb7c2c38d41c7173afd
ChakkritGit <nongtonnee@gmail.com>
2025-05-30 10:04:50 +0700

    Update version number to 2.0.0b27e in Sidebar component and adjust body styles in CSS

--- src/components/navigation/sidebar/smtrack/sidebar.tsx
+++ src/components/navigation/sidebar/smtrack/sidebar.tsx
@@ -344,7 +344,7 @@ const Sidebar = () => {
                 isExpand ? 'text-center' : 'text-right'
               } hover:underline cursor-pointer`}
             >
-              Version 2.0.0b27d
+              Version 2.0.0b27e
             </Link>
           </div>
         </div>
