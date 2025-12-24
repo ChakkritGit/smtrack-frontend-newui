1b62286791d4ac7019ba60bb32037e04966ff923
ChakkritGit <nongtonnee@gmail.com>
2025-12-24 14:02:57 +0700

    change title name edit device tms

--- src/components/navigation/sidebar/smtrack/sidebar.tsx
+++ src/components/navigation/sidebar/smtrack/sidebar.tsx
@@ -344,7 +344,7 @@ const Sidebar = () => {
                 isExpand ? 'text-center' : 'text-right'
               } hover:underline cursor-pointer`}
             >
-              Version 2.0.3
+              Version 2.0.4
             </Link>
           </div>
         </div>
