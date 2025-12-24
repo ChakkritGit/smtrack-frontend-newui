4a7f43c01508d96e79ce0ca7bb4b63e8787b61ae
ChakkritGit <nongtonnee@gmail.com>
2025-12-24 13:43:17 +0700

    change version

--- src/components/navigation/sidebar/smtrack/sidebar.tsx
+++ src/components/navigation/sidebar/smtrack/sidebar.tsx
@@ -344,7 +344,7 @@ const Sidebar = () => {
                 isExpand ? 'text-center' : 'text-right'
               } hover:underline cursor-pointer`}
             >
-              Version 2.0.2
+              Version 2.0.3
             </Link>
           </div>
         </div>
