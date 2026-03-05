6f3ab84a83cf64d1528007a074b43d56db772bad
ChakkritGit <nongtonnee@gmail.com>
2026-03-05 23:15:59 +0700

    fix env variant

--- src/components/navigation/sidebar/smtrack/sidebar.tsx
+++ src/components/navigation/sidebar/smtrack/sidebar.tsx
@@ -335,7 +335,7 @@ const Sidebar = () => {
                 isExpand ? "text-center" : "text-right"
               } hover:underline cursor-pointer`}
             >
-              Version 2.0.18
+              Version 2.0.18a
             </Link>
           </div>
         </div>
